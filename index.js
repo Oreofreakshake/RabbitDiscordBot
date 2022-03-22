d4b0406522111b6ffc2681c6483f73797ef9183e
Oreofreakshake <53213763+Oreofreakshake@users.noreply.github.com>
2022-03-22 16:40:41 +0500

    dynamic command handing works

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
+const fs = require("fs");
+
 const Discord = require("discord.js");
 require("dotenv").config();
 
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
-});
+}); //making the client
+
+client.commands = new Discord.Collection(); //mapping the commands setup
+
+//--------------------------------------------
+
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js")); //getting the array of the files in commmands dir
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`); //iterating the array
+
+    client.commands.set(command.name, command); //setting the name for the command from the file name
+}
+
+//--------------------------------------------
 
 client.on("ready", () => {
     const channel = client.channels.cache.get("926987961868890162");
-    const text = `<@!442629841716772864> bought me back online!`;
+    const text = `${process.env.OWNER} bought me back online!`;
     console.log("ready");
     channel.send(text);
+});
+
+client.on("messageCreate", (message) => {
+    //this function will run on message commands dynamically from the command dir
+    const prefix = process.env.PREFIX;
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    } //check for prefix
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase(); //removing prefix and taking the command only
+
+    if (!client.commands.has(command)) {
+        return;
+    }
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.log(error);
+        message.reply("seems like there's an issue");
+    }
 });
 
 token = `${process.env.TOKEN}`;
